9c3e5649f82dc35c2d3aa5f90793a63e1ffb4c89
scrumpygeo <scrumpygeo@yahoo.co.uk>
2020-02-03 13:17:44 +0000

    Added simple error handling

--- src/index.js
+++ src/index.js
@@ -6,18 +6,29 @@ class App extends React.Component {
     super(props);
 
     // initialize state object as null; = only time we do direct assignment of state
-    this.state = { lat: null };
+    this.state = { lat: null, errorMessage: "" };
 
     // place this here, not in render() so only gets called once.
     window.navigator.geolocation.getCurrentPosition(
-      position => console.log(position), // success callback
-      err => console.log(err)
+      position => {
+        this.setState({ lat: position.coords.latitude });
+      }, // success callback
+      err => {
+        this.setState({ errorMessage: err.message });
+      }
     );
   }
 
   // render() method is obligatory in React
   render() {
-    return <div>Latitude: {this.state.lat}</div>;
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+    if (this.state.lat && !this.state.errorMessage) {
+      return <div>Latitude: {this.state.lat}</div>;
+    }
+
+    return <div>Loading...</div>;
   }
 }
 
