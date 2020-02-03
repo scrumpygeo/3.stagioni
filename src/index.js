cf47743032936c313f353f413f54c5041ab500b8
scrumpygeo <scrumpygeo@yahoo.co.uk>
2020-02-03 11:38:50 +0000

    move getCuttentPosition to constructor from render method

--- src/index.js
+++ src/index.js
@@ -2,13 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 class App extends React.Component {
-  render() {
+  constructor(props) {
+    super(props);
+
+    // initialize state object as null; = only time we do direct assignment of state
+    this.state = { lat: null };
+
+    // place this here, not in render() so only gets called once.
     window.navigator.geolocation.getCurrentPosition(
       position => console.log(position), // success callback
       err => console.log(err)
     );
+  }
 
-    return <div>Latitude:</div>;
+  // render() method is obligatory in React
+  render() {
+    return <div>Latitude: {this.state.lat}</div>;
   }
 }
 
